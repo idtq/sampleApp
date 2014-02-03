5a81fd10f3d68ecd4f45706109b66a9b42b037b8
melike.ilteralp <milteralp@gmail.com>
2014-02-03 16:26:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dakika23::Application.config.secret_token = 'c0244c4b64b31edef9185168ff3e760e3ef9e89c9490315a09b05e41be454357e0b447f2f495bffc4d64395a9c07a71b23b83d797b81eb61edcc066b196f8bf2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Dakika23::Application.config.session_store :cookie_store, key: '_dakika_23_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Dakika23::Application.config.session_store :active_record_store
